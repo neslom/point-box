c8b99c44ba9573a71877642086d8dc10f0d0ec76
Markus Olsen <molsen13@gmail.com>
2015-03-06 22:10:03 -0700

    User model is invalid with missing password or username.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create" do
+    let(:user) { User.create(username: "molsen", password: "password", available_points: 10, redeemed_points: 0) }
+
+    it "is valid with a username and password" do
+      expect(user).to be_valid
+    end
+
+    it "is invalid with missing username" do
+      user.username = nil
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid with missing password" do
+      user.password = nil
+
+      expect(user).to_not be_valid
+    end
+  end
 end
