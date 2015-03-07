1ac14a7ea85f723eb33c30340df5e9597deecd95
Markus Olsen <molsen13@gmail.com>
2015-03-07 14:44:25 -0700

    User dashboard shows redeemed points.

--- spec/features/user_dashboard_spec.rb
+++ spec/features/user_dashboard_spec.rb
@@ -2,12 +2,17 @@ require 'rails_helper'
 
 RSpec.describe "User dashboard" do
   let(:user) { User.create(username: "markus", password: "password", available_points: 50, redeemed_points: 100) }
+  before(:each) { login_as(user) }
 
-  it "shows available and redeemed points" do
-    login_as(user)
-
+  it "shows available points" do
     within("#user_points") do
       expect(page).to have_content("Available Points: 50")
+    end
+  end
+
+  it "shows redeemed points" do
+    within("#user_points") do
+      expect(page).to have_content("Redeemed Points: 100")
     end
   end
 end
