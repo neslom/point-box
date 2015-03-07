193ee82134d88e0cb7a56e65d432409d31478585
Markus Olsen <molsen13@gmail.com>
2015-03-07 09:52:06 -0700

    Pre-admin auth tests passing

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -1,43 +1,53 @@
 require 'rails_helper'
 
 RSpec.feature "User login" do
-  scenario "logs in with valid username and password" do
-    user = User.create(username: "molsen", password: "password", available_points: 10, redeemed_points: 0)
+  context "as default role" do
+    scenario "logs in with valid username and password" do
+      user = User.create(username: "molsen", password: "password")
 
-    visit login_path
+      visit login_path
 
-    fill_in "session[username]", with: user.username
-    fill_in "session[password]", with: user.password
-    click_link_or_button "Log in"
+      fill_in "session[username]", with: user.username
+      fill_in "session[password]", with: user.password
+      click_link_or_button "Log in"
 
-    expect(current_path).to eq(user_path(user))
-    expect(page).to have_content("Welcome, molsen!")
+      expect(current_path).to eq(user_path(user))
+      expect(page).to have_content("Welcome, molsen!")
+      expect(page).not_to have_content("Invalid login")
+    end
+
+    scenario "login fails with invalid username" do
+      user = User.create(username: nil, password: "password")
+
+      visit login_path
+
+      fill_in "session[username]", with: user.username
+      fill_in "session[password]", with: user.password
+      click_link_or_button "Log in"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Invalid login")
+    end
+
+    scenario "login fails with invalid password" do
+      user = User.create(username: nil, password: "password")
+
+      visit login_path
+
+      fill_in "session[username]", with: user.username
+      fill_in "session[password]", with: user.password
+      click_link_or_button "Log in"
+
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content("Invalid login")
+    end
   end
 
-  scenario "login fails with invalid username" do
-    user = User.create(username: nil, password: "password", available_points: 10, redeemed_points: 0)
+  context "as admin role" do
+    scenario "is taken to admin dashboard" do
+      user = User.create(username: "Bob", password: "password", role: 1)
 
-    visit login_path
-
-    fill_in "session[username]", with: user.username
-    fill_in "session[password]", with: user.password
-    click_link_or_button "Log in"
-
-    expect(current_path).to eq(login_path)
-    expect(page).to have_content("Invalid login")
-  end
-
-  scenario "login fails with invalid password" do
-    user = User.create(username: nil, password: "password", available_points: 10, redeemed_points: 0)
-
-    visit login_path
-
-    fill_in "session[username]", with: user.username
-    fill_in "session[password]", with: user.password
-    click_link_or_button "Log in"
-
-    expect(current_path).to eq(login_path)
-    expect(page).to have_content("Invalid login")
-
+      expect(user.role).to eq("admin")
+    end
   end
 end
