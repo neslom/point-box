0cddf854ff2af7da824e17bdb2f094ef8af600e0
Markus Olsen <molsen13@gmail.com>
2015-03-08 15:11:21 -0600

    User can click on 'Redeem Reward' link and it is redeemed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
   end
+
+  def update
+  end
 end
--- spec/features/reward_index_spec.rb
+++ spec/features/reward_index_spec.rb
@@ -1,26 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe "Rewards index page" do
-  Reward.create(title: "Car", worth: 1000)
-  Reward.create(title: "Sandwich", worth: 50)
-  let(:user) { User.create(username: "markus", password: "password") }
+  let!(:reward_1) { Reward.create(title: "Car", worth: 1000) }
+  let!(:reward_2) { Reward.create(title: "Sandwich", worth: 50) }
+  let(:user) { User.create(username: "markus", password: "password", available_points: 1000) }
 
-  xit "has links to the rewards" do
+  before(:each) do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
     visit rewards_path
-
-    expect(page).to have_link("Redeem Reward", href: user_path(user))
-    # will need to change above expectation
-    #
-    # might be best to RedeemedReward.create(user_id: params[:id], reward_id: reward.id)
-    #   so the same reward isn't created in the Rewards table and same for Users
-    #
-    # of course will have to make sure the available points are there on the user, then convert
-    # those points to redeemed if the join table entry is indeed created
-    #
-    # ALSO the Reward.create calls within the main describe block are stil present in the test DB
-    # after the test. They are being cleaned out before each test run.
-
   end
+
+  context "before the 'redeem reward' link has been clicked" do
+    it "has links to the rewards" do
+      expect(page).to have_link("Redeem Reward", href: redeemed_rewards_path(reward_id: reward_1.id, user_id: user.id))
+      expect(page).to have_link("Redeem Reward", href: redeemed_rewards_path(reward_id: reward_2.id, user_id: user.id))
+    end
+  end
+
+  context "when the 'redeem reward' link has been clicked" do
+    context "when the User has enough available points" do
+      it "creates redeemed reward" do
+        expect(RedeemedReward.count).to eq(0)
+
+        find_link("Redeem Reward", href: redeemed_rewards_path(reward_id: reward_2.id, user_id: user.id)).click
+
+        within("#flash_notice") do
+          expect(page).to have_content("The reward is now yours")
+        end
+
+        expect(RedeemedReward.count).to eq(1)
+        expect(current_path).to eq(user_path(user))
+      end
+
+
+    end
+  end
+
 end
